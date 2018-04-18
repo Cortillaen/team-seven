b6d851fe7f76f0953852f693ad844b77f72544fd
ltv115 <lyle.vanfossan@gmail.com>
2018-04-18 14:47:00 -0500

    Created some Html for file as well as added eventRegistry dependendence

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import About from '@/components/About'
+import Ratings from '@/components/Ratings'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/ratings',
+      name: 'Ratings',
+      component: Ratings
     }
   ]
 })
